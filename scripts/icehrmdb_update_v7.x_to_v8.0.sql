51d449a367ac9791212ffba8c3815bf33cc61394
Thilina Hasantha <thilina.hasantha@gmail.com>
2014-12-22 05:09:44 +0530

    Table name corrected

--- scripts/icehrmdb_update_v7.x_to_v8.0.sql
+++ scripts/icehrmdb_update_v7.x_to_v8.0.sql
@@ -4,6 +4,6 @@ ALTER TABLE `Attendance` ADD KEY `employee_in_time` (`employee`,`in_time`);
 ALTER TABLE `Attendance` ADD KEY `employee_out_time` (`employee`,`out_time`);
 
 
-ALTER TABLE ``EmployeeTimeEntry`` ADD KEY `employee_project` (`employee`,`project`);
-ALTER TABLE ``EmployeeTimeEntry`` ADD KEY `employee_project_date_start` (`employee`,`project`,`date_start`);
+ALTER TABLE `EmployeeTimeEntry` ADD KEY `employee_project` (`employee`,`project`);
+ALTER TABLE `EmployeeTimeEntry` ADD KEY `employee_project_date_start` (`employee`,`project`,`date_start`);
 

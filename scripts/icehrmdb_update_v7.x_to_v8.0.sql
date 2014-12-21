f9fe91dfb05a224ae3506593e05b7b5267556abc
Thilina Hasantha <thilina.hasantha@gmail.com>
2014-12-22 05:28:59 +0530

    Fix table name issue

--- scripts/icehrmdb_update_v7.x_to_v8.0.sql
+++ scripts/icehrmdb_update_v7.x_to_v8.0.sql
@@ -7,3 +7,8 @@ ALTER TABLE `Attendance` ADD KEY `employee_out_time` (`employee`,`out_time`);
 ALTER TABLE `EmployeeTimeEntry` ADD KEY `employee_project` (`employee`,`project`);
 ALTER TABLE `EmployeeTimeEntry` ADD KEY `employee_project_date_start` (`employee`,`project`,`date_start`);
 
+
+REPLACE INTO `Reports` (`id`, `name`, `details`, `parameters`, `query`, `paramOrder`, `type`) VALUES
+(5, 'Employee Time Tracking Report', 'This report list employee working hours and attendance details for each day for a given period ', '[\r\n[ "employee", {"label":"Employee","type":"select2","allow-null":false,"remote-source":["Employee","id","first_name+last_name"]}],\r\n[ "date_start", {"label":"Start Date","type":"date"}],\r\n[ "date_end", {"label":"End Date","type":"date"}]\r\n]', 'EmployeeTimeTrackReport', '["employee","date_start","date_end"]', 'Class');
+
+
